da74579d303bef0596d13abffe78955b13412b60
Nathan Kerr <nathan@nathankerr.com>
2012-12-14 11:07:31 +0100

    Added build instructions, updated testcount.c to adapt from removal of clusterGIS_rank and clusterGIS_tasks which were removed at some point

--- tests/testcount.c
+++ tests/testcount.c
@@ -9,6 +9,8 @@ int main(int argc, char** argv) {
 	int previous_id;
 	int first_id;
 	MPI_Status status;
+	int rank;
+	int tasks;
 
 	/* Process local arguments */
 	if (argc != 2) {
@@ -17,7 +19,10 @@ int main(int argc, char** argv) {
 	}
 
 	clusterGIS_Init(&argc, &argv);
-	clusterGIS_Load_data_distributed(argv[1], &dataset);
+	dataset = clusterGIS_Load_csv_distributed(MPI_COMM_WORLD, argv[1]);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &tasks);
 
 	record = dataset->data;
 	last_id = atoi(record->data[0]) - 1;
@@ -25,7 +30,7 @@ int main(int argc, char** argv) {
 	while(record != NULL) {
 		count++;
 		if(atoi(record->data[0]) != last_id + 1) {
-			printf("%d: MISSING RECORD %d\n", clusterGIS_rank, last_id + 1);
+			printf("%d: MISSING RECORD %d\n", rank, last_id + 1);
 		}
 		last_id = atoi(record->data[0]);
 		record = record->next;
@@ -33,26 +38,26 @@ int main(int argc, char** argv) {
 
 	MPI_Reduce(&count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-	if(clusterGIS_rank == 0) {
+	if(rank == 0) {
 		printf("Count: %d\n", total_count);
 	}
 
-	if (clusterGIS_tasks > 1) {
-		if (clusterGIS_rank == 0) {
-			MPI_Send(&last_id, 1, MPI_INT, clusterGIS_rank+1, 99, MPI_COMM_WORLD);
+	if (tasks > 1) {
+		if (rank == 0) {
+			MPI_Send(&last_id, 1, MPI_INT, rank+1, 99, MPI_COMM_WORLD);
 			previous_id = 0;
-		} else if (clusterGIS_rank == clusterGIS_tasks - 1) {
-			MPI_Recv(&previous_id, 1, MPI_INT, clusterGIS_rank-1, 99, MPI_COMM_WORLD, &status);
+		} else if (rank == tasks - 1) {
+			MPI_Recv(&previous_id, 1, MPI_INT, rank-1, 99, MPI_COMM_WORLD, &status);
 		} else {
-			MPI_Sendrecv(&last_id, 1, MPI_INT, clusterGIS_rank+1, 99, &previous_id, 1, MPI_INT, clusterGIS_rank-1, 99, MPI_COMM_WORLD, &status);
+			MPI_Sendrecv(&last_id, 1, MPI_INT, rank+1, 99, &previous_id, 1, MPI_INT, rank-1, 99, MPI_COMM_WORLD, &status);
 		}
 		first_id = atoi(dataset->data->data[0]);
 		if(previous_id == first_id) {
-			printf("Record %d is duplicated between tasks %d and %d\n", previous_id, clusterGIS_rank - 1, clusterGIS_rank);
+			printf("Record %d is duplicated between tasks %d and %d\n", previous_id, rank - 1, rank);
 		} else 	if(previous_id + 1 != first_id) {
-			printf("Record %d is missing between tasks %d and %d\n", previous_id + 1, clusterGIS_rank -1, clusterGIS_rank);
+			printf("Record %d is missing between tasks %d and %d\n", previous_id + 1, rank -1, rank);
 		}
-		//printf("%d: %d, %s - %s\n", clusterGIS_rank, previous_id, dataset->data->data[0], last_record->data[0]);
+		//printf("%d: %d, %s - %s\n", rank, previous_id, dataset->data->data[0], last_record->data[0]);
 	}
 	
 	/* Finalize */
